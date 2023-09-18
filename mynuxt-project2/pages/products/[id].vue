ac51007685e1b000422c88b51b91b5ebb761cae9
“Prin <tae17688@gmail.com>
2023-09-18 11:27:35 +0700

    Firsoading code

---
+++ mynuxt-project2/pages/products/[id].vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <ProductDetails :product="product" />
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+    const uri = 'https://fuzik03.tetraserver.com/compactdisc?album=' + id
+
+    // fetch singlle product
+    const { data: product } = await useFetch(uri, { key: id})
+
+    if(!product.value) {
+        throw createError({ statusCode: 404, statusMessage: 'Product not found'})
+    }
+
+    definePageMeta({
+        layout:'products'
+    })
+    
+</script>
+
+<style scoped>
+
+</style>
