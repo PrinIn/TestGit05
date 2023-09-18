879fde73f1b018a8df09ca9f16d26b3b9bdee33b
“Prin <tae17688@gmail.com>
2023-09-18 11:15:20 +0700

    First try - uploadinge

---
+++ mynuxt-project/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem excepturi earum eos, 
+            doloribus eligendi et odit cumque
+             sunt rerum nam, voluptate non cupiditate libero! Nisi iure alias voluptatem fugiat earum!</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
